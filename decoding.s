f55ba537d1be293083690fd9cda2b3eeff6b3d91
Michael Harkins <mph47@case.edu>
2013-03-19 17:13:15 -0400

    finished encoding and decoding
    blah

--- decoding.s
+++ decoding.s
@@ -20,7 +20,6 @@ move $t7, $a0
 la $a0, sWord
 li $v0,4
 syscall
-
 add $t4, $zero,$zero
 calcSize:
 lb $t2, sWord($t4)
@@ -28,8 +27,10 @@ beq $t2, $zero, getKey
 addi $t3, $t3, 1
 addi $t4,$t4,1
 b calcSize
-
 getKey:
+add $t9, $zero, $zero
+addi $t3, $t3, -1
+add $t4, $zero,$zero
 li $v0,4
 la $a0, key
 syscall
@@ -40,27 +41,27 @@ calculateBitAdd:
 li $s1,7
 div $s0, $s1
 mfhi $t5
+add $s7, $t5, $zero
+initPowerloop:
 li $s2, 2
 li $s3, 2
+beq $s7, $zero, resetCount
+add $t5, $s7,$zero
 powerloop:
-beq $t5, $zero, initAddBits
+beq $t5, $zero, addBits
 mult $s2,$s3
 mflo $s2
 addi $t5,$t5, -1
 b powerloop
-
-initAddBits:
-add $t4, $zero,$zero
-addi $t3, $t3, -1
 addBits:
-beq $t5,$t3,printInit
+beq $t9,$t3,printInit
 lb $s4, sWord($t4)
 xor $s5, $s2, $s4
 sb $s5, sWord($t4)
-
-addi $t5, $t5, 1
+addi $t9, $t9, 1
 addi $t4, $t4, 1
-b addBits
+addi $s7, $s7, -1
+b initPowerloop
 
 printInit:
 add $t4, $zero,$zero
@@ -74,4 +75,7 @@ b print
 e:
 li $v0, 10
 syscall
+resetCount:
+li $s7, 7
+b initPowerloop
 # END OF PROGRAM
